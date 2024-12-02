4d1709321da7bf9ef1ca0c14b03ab88ce640ca73
chammi2107 <sachinkumar2107@outlook.com>
2024-12-02 16:52:50 +0530

    Update install-changelog.sql

--- stg/install/install-changelog.sql
+++ stg/install/install-changelog.sql
@@ -2,7 +2,7 @@
 
 -- changeset DP:1
 -- Purpose: Create the initial example_table with primary key and constraints
-CREATE TABLE liquibase_test.liquibase_table (
+CREATE TABLE install.liquibase_table (
     id INT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description VARCHAR(255),
@@ -13,7 +13,7 @@ CREATE TABLE liquibase_test.liquibase_table (
 
 -- changeset sachin:2
 -- Purpose: Add a new table
-CREATE TABLE liquibase_test.sachin_test (
+CREATE TABLE install.liquibase_table (
     id INT PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
